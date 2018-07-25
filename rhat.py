c46fefc6eeaf356c94f5da02c1ec2a83fcb0d267
swallstrom <swallstrom@asiaa.sinica.edu.tw>
2018-07-25 17:36:42 +0800

    Fixed more errors in rhat

--- rhat.py
+++ rhat.py
@@ -17,20 +17,21 @@ def rhat_function(theta):
 	nchains = ntheta[1]
 	npar = ntheta[2]
 
-	Bvar = np.array()
-	Wvar = np.array()
+	Bvar = np.zeros(npar)
+	Wvar = np.zeros(npar)
 
 	for j in range(npar):
+        #sum over axis 0 (niter) for all nchains and npar = j. Output is a vector of length nchains
 		thetabarj = np.sum(theta[:,:,j],0) / niter
 		thetabar  = np.mean(thetabarj)
 
 		sjsqr = 0.
 
 		for i in range(nchains):
-			sjsqr = sjsqr + np.sum(theta[:,i,j] - thetabarj[0][i]**2) / (niter - 1)
+			sjsqr = sjsqr + np.sum(theta[:,i,j] - thetabarj[i]**2) / (niter - 1)
 
-        np.insert(Bvar,j,niter / (nchains - 1.0) * np.sum( (thetabarj - thetabar)^2 ))
-        np.insert(Wvar,j,sjsqr / nchains)
+        Bvar[j] = niter / (nchains - 1.0) * np.sum((thetabarj - thetabar)^2)
+        Wvar[j] = sjsqr / nchains
 
 	varplus = (1 - 1.0 / niter) * Wvar + Bvar / niter
 	rhat = np.sqrt(varplus/Wvar)
